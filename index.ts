52e0b0378905c59a9e68a49758f0c057ff4ace0a
Nirmal Savinda <nirmalsavinda29@gmail.com>
2025-11-08 11:01:43 +0530

    Change welcome message in index.ts

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 const port = process.env.PORT || 8210;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+  res.send('Nirmal Savinda - 25990090');
 });
 
 app.listen(port, () => {
